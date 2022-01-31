85bb468fad6ac2c69936eacd82dfbe7c347a0908
Sidharth Das <sdas.engineer@gmail.com>
2022-01-31 01:31:02 +0000

    Comment dotenv

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,11 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-if ['development', 'test'].include? ENV['RAILS_ENV']
-Dotenv::Railtie.load
-end
 
-HOSTNAME = ENV['HOSTNAME']
+# Dotenv::Railtie.load
+
+
+# HOSTNAME = ENV['HOSTNAME']
 
 
 module Ceefuu
