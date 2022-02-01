584597151a0ef2add822053e4d07a1c0edb56629
Sidharth Das <sdas.engineer@gmail.com>
2022-02-01 12:18:30 +0000

    Updated SES and forced SSL

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,21 +2,21 @@ class OrderMailer < ApplicationMailer
 
   def order_buyer_email
     buyer_user = params[:buyer_user]
-    mail(to: buyer_user.email, subject: 'Order Placed')
+    mail(to: buyer_user.email, subject: 'A new order has been placed : Ceefuu')
   end
 
   def order_creator_email
     creator_user = params[:creator_user]
-    mail(to: creator_user.email, subject: 'Order Created')
+    mail(to: creator_user.email, subject: 'A new order has been created : Ceefuu')
   end
 
   def mark_as_complete_buyer_email
     @order = params[:order]
-    mail(to: @order.buyer.email, subject: 'You completed order')
+    mail(to: @order.buyer.email, subject: 'Your order has been completed : Ceefuu')
   end
 
   def mark_as_complete_creator_email
     @order = params[:order]
-    mail(to: @order.creator.email, subject: 'Your order completed by buyer')
+    mail(to: @order.creator.email, subject: 'Your order has been approved by buyer : Ceefuu')
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
@@ -119,6 +119,22 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {
+    host: 'ceefuu.com',
+    protocol: 'https'
+  }
+
+  config.action_mailer.smtp_settings = {
+    address: Rails.application.credentials.dig(:aws, :ses_server),
+    port: 587,
+    user_name: Rails.application.credentials.dig(:aws, :ses_username),
+    password: Rails.application.credentials.dig(:aws, :ses_password),
+    authentication: :login,
+    enable_starttls_auto: true
+  }
 
   config.hosts = nil
 end
