316748f508430016afa850619d154535f7bdfb61
Sidharth Das <sdas.engineer@gmail.com>
2022-02-11 02:48:11 +0000

    Reversed ssh

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,3 @@
 #FIXME replace 1.2.3.4 with your IP address
 server '34.224.81.193', user: 'deploy', roles: %w{web app db}
-
- set :ssh_options, {
-   keys: %w(/home/deploy/.ssh/id_rsa),
-   forward_agent: false,
-   auth_methods: %w(password)
- }
- 
 set :rails_env, 'production'
